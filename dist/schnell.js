c2f328f6a6b4ef96f2bb8dae30e919718b30b290
Gabriele Bianchet-David <g.bianchetdavid@gmail.com>
2017-02-25 12:45:28 -0500

    Created very basic REPL (only responds with input)

--- dist/schnell.js
+++ dist/schnell.js
@@ -1,1 +1,31 @@
-console.log("Welcome to Schnell");
+/*
+ * Main File to set up the CLI.
+ */
+
+"use strict";
+// Import Readline module to read lines from command line
+const readLine = require('readline');
+// Import OS for cross platform EOL
+const os = require('os');
+// Defining cross-plat constant for EOL
+let EOL = os.EOL;
+// Defining prompt
+let prompt = "$ ";
+// Create the CLI instance
+let rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    completer: null
+});
+// Set the prompt
+rl.setPrompt(prompt, prompt.length);
+
+rl.prompt();
+
+rl.on('line', (line) => {
+    console.log(line.trim());
+    rl.prompt();
+}).on('close', () => {
+    console.log(EOL + "Exiting...");
+    process.exit(0);
+});
