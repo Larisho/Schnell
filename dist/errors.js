365e1e09a7d44108a2ba5b3e0f47d748d519c1db
Gabriele Bianchet-David <g.bianchetdavid@gmail.com>
2017-02-26 01:13:21 -0500

    shell now has a basic parser and tokenizer

--- dist/errors.js
+++ dist/errors.js
@@ -1,5 +1,37 @@
-"use strict";
-
 /*
  * Implementation of custom error classes.
  */
+
+"use strict";
+
+class BashError extends Error {
+    constructor(message, errorHint) {
+        super(message);
+
+        this.name = this.constructor.name;
+        if (errorHint)
+            this.errorHint = errorHint;
+    }
+
+    printError() {
+        console.log(this.message + ': ' + this.errorHint);
+    }
+}
+
+class SyntaxError extends BashError {
+    constructor(message, where) {
+        super(message);
+
+        this.name = this.constructor.name;
+        this.errorHint = where;
+    }
+
+    printError() {
+        console.log(this.message + ': [ ' + this.errorHint + ' ]');
+    }
+}
+
+exports = module.exports = {
+    BashError,
+    SyntaxError
+};
--- dist/parse.js
+++ dist/parse.js
@@ -4,69 +4,74 @@
 
 "use strict";
 
-function Parser() {
-    if (!(this instanceof Parser)) {
-        return new Parser();
+const errors = require('./errors');
+
+class Parser {
+    constructor() {
+        // Argument vector
+        this.argv = ["Nothing parsed"];
+
+        // Argument count
+        this.argc = 0;
     }
 
-    /*
-     // Argument vector
-     this.argv = [];
+    parse(userInput) {
+        // tokenize the input
+        let raw = userInput.split(" ");
+        let argv = [];
 
-     // Argument count
-     this.argc = 0;
-     */
+        for (let i = 0; i < raw.length; i++) {
 
-}
+            // If argv[i] is the beginning of a string make the
+            // whole string a token
+            if (raw[i][0] === '"' || raw[i][0] === "'") {
 
-Parser.prototype.parse = function (userInput) {
-    // tokenize the input
-    let raw = userInput.split(" ");
-    let argv = [];
-
-    for (let i = 0; i < raw.length; i++) {
-
-        // If argv[i] is the beginning of a string make the
-        // whole string a token
-        if (raw[i][0] === '"' || raw[i][0] === "'") {
-
-            // Save the appropriate delimiter
-            let artifact = raw[i][0];
-            // Build string here
-            let totalString = '';
-            // Get length of word at 'i'
-            let length = raw[i].length - 1;
-
-            // Iterate through the rest of the text until
-            // a matching operator is found.
-            while (raw[i][length] !== artifact) {
-
-                // Append next word to total string
-                totalString += raw[i] + ' ';
-                // Increment 'i'
-                i++;
-
-                // If that operator is not found,
-                // throw a syntax error.
-                if (i >= raw.length) {
-                    // throw error!
-                    console.log('You done goofed!');
-                    process.exit(1);
+                if (raw[i + 1] === undefined) {
+                    throw new errors.SyntaxError("Unterminated String", raw.join(' '));
                 }
 
-                // Get length of next word
-                length = raw[i].length - 1;
-            }
-            totalString += raw[i];
-            argv.push(totalString);
-            console.log("Just pushed and this is i: ${i}", i);
-        }
-        else {
-            argv.push(raw[i]);
-        }
-    }
+                // Save the appropriate delimiter
+                let artifact = raw[i][0];
+                // Build string here
+                let totalString = '';
+                // Get length of word at 'i'
+                let length = raw[i].length - 1;
 
-    console.log(argv);
-};
+                // Iterate through the rest of the text until
+                // a matching operator is found.
+                while (raw[i][length] !== artifact) {
+
+                    // Append next word to total string
+                    totalString += raw[i] + ' ';
+                    // Increment 'i'
+                    i++;
+
+                    // If that operator is not found,
+                    // throw a syntax error.
+                    if (i >= raw.length) {
+                        // throw error!
+                        throw new errors.SyntaxError("Unterminated String", totalString);
+                    }
+
+                    // Get length of next word
+                    length = raw[i].length - 1;
+                }
+                // Add final piece of string to total
+                totalString += raw[i];
+                // Push new token onto argument vector
+                argv.push(totalString);
+            }
+            else {
+                // Push new token onto argument vector
+                argv.push(raw[i]);
+            }
+        }
+
+        this.argv = argv.filter((val) => {
+            return val !== '';
+        });
+        this.argc = this.argv.length;
+    }
+}
 
 exports = module.exports = Parser;
