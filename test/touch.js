9f877dbc718bfdf0ae34505bb0239d5b17a9516c
Gabriele Bianchet-David <g.bianchetdavid@gmail.com>
2017-05-24 23:50:54 -0400

    Finished fleshing out the unit tests for the touch command

--- test/touch.js
+++ test/touch.js
@@ -24,4 +24,32 @@ module.exports = function() {
         builtins.touch([newFile, '-c']);
         fs.existsSync(process.cwd() + path.sep + newFile).should.equal(false);
     });
+
+    it('should change the access time of a file to the current time on the "-a" flag', function() {
+
+    });
+
+    it('should change the modification time of a file to the current time on the "-m" flag', function() {
+
+    });
+
+    it('should change the access and modification time of a file based on the values for a given file (-r)', function() {
+
+    });
+
+    it('should change the access and modification time of a file based on a given input string (-t)', function() {
+
+    });
+
+    it('should fail when no file names are supplied', function() {
+
+    });
+
+    it('should fail when the "-t" flag argument is incorrectly written', function() {
+
+    });
+
+    it('should be able to create multiple new files when many file names are supplied', function() {
+
+    });
 };
